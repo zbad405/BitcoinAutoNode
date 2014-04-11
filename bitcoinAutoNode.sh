c2a47083cc5ecfa95e5458ae1efc3a3ce45e05d4
TheZ3ro <silvethebest@yahoo.it>
2014-04-11 22:17:24 +0200

    Zetacoin Node

--- bitcoinAutoNode.sh
+++ bitcoinAutoNode.sh
@@ -1,39 +1,42 @@
 #!/bin/bash
-echo "########### The server will reboot when the script is complete"
-echo "########### Changing to home dir"
-cd ~
-echo "########### Change your root password!"
-passwd
-echo "########### Firewall rules; allow 22,8333"
-ufw allow 22/tcp
-ufw allow 8333/tcp
+# Made by TheZero
+#
+# Dependency
+#apt-get install build-essential libssl-dev libboost-all-dev libdb4.8-dev libdb4.8++-dev git
+#
+
+echo "- Started! -"
+cd /opt
+
+#Download Zetacoin
+git clone https://github.com/zetacoin/zetacoin.git
+cd zetacoin/src/
+make -f makefile.unix 
+ln -s /opt/zetacoin/src/zetacoind /usr/bin/
+
+#Linux Firewall rule
+ufw allow 17333/tcp
 ufw --force enable
-echo "########### Updating Ubuntu"
-apt-get update -y
-apt-get upgrade -y
-apt-get dist-upgrade -y
-apt-get install software-properties-common python-software-properties -y
-echo "########### Creating Swap"
+
+# Creating Swap
 dd if=/dev/zero of=/swapfile bs=1M count=1024 ; mkswap /swapfile ; swapon /swapfile
 echo "/swapfile swap swap defaults 0 0" >> /etc/fstab
-echo "########### Adding ppa:bitcoin/bitcoin and installing bitcoind"
-add-apt-repository -y ppa:bitcoin/bitcoin
-apt-get update -y
-mkdir ~/.bitcoin/
-apt-get install bitcoind -y
-echo "########### Creating config"
-config=".bitcoin/bitcoin.conf"
+
+# Creating config
+config=".zetacoin/zetacoin.conf"
 touch $config
 echo "server=1" > $config
 echo "daemon=1" >> $config
 echo "connections=40" >> $config
+echo "rpcallowip=127.*.*.*" >> $config
+echo "rpcport=17335" >> $config
+echo "port=17333" >> $config
+echo "listen=1" >> $config
+echo "txindex=1" >> $config
 randUser=`< /dev/urandom tr -dc A-Za-z0-9 | head -c30`
 randPass=`< /dev/urandom tr -dc A-Za-z0-9 | head -c30`
 echo "rpcuser=$randUser" >> $config
 echo "rpcpassword=$randPass" >> $config
-echo "########### Setting up autostart (cron)"
-crontab -l > tempcron
-echo "@reboot bitcoind -daemon" >> tempcron
-crontab tempcron
-rm tempcron
-reboot
+
+# Ok, we are ready :)
+nohup zetacoind &
